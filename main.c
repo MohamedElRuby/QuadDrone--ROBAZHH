22a339030e7c95c5e989f45046370ee840fd2067
Mohamed ElRuby <mohamed.a.alsayedd@gmail.com>
2023-05-06 11:27:45 +0300

    edit in main.c headers

--- main.c
+++ main.c
@@ -6,8 +6,8 @@
  */
 #include "BIT_MATH.h"
 #include "STD_TYPES.h"
-#include "DIO_interface.h"
-#include "Timer_interface.h"
+#include "MCAL/DIO_interface.h"
+#include "MCAL/Timer_interface.h"
 #include <util/delay.h>
 
 void main(){
@@ -16,7 +16,7 @@ void main(){
 	TIM1_voidSetTimerReg(20000);
 	u16 angleTime;
 	DIO_SetPortDirection(PORTD,PORT_OUTPUT);
-	TIM1_A_voidSetCompareVal(1000);
+	TIM1_A_voidSetCompareVal(1020);
 }
 
 
